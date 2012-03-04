610efa820e577681620466abe136b437766b3ebe
Albert <albertyfwu@gmail.com>
2012-03-04 05:20:43 -0500

    fixed layout

--- src/main.py
+++ src/main.py
@@ -4,39 +4,26 @@ import jinja2
 
 from google.appengine.api import users
 
+from apiclient.discovery import build
+
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname
 (__file__)))
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        
-        
         user = users.get_current_user()
         
-        if user:        
+        if user:
             template_values = {
-                'username': user.nickname()
+                'username': user.nickname(),
+                'logOutUrl': users.create_logout_url("/")
             }
             
             template = jinja_environment.get_template('index.html')
             self.response.out.write(template.render(template_values))
         else:
             self.redirect(users.create_login_url(self.request.uri))
-        
-#        user = users.get_current_user()
-#
-#        if user:
-#            self.response.headers['Content-Type'] = 'text/plain'
-#            self.response.out.write('Hello, ' + user.nickname())
-#        else:
-#            self.redirect(users.create_login_url(self.request.uri))
 
-class TestPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('testing')
-
-app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/testing', TestPage)],
+app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
