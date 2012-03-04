c022ead2c4896c7ca4996030a909ccf841db8930
Albert <albertyfwu@gmail.com>
2012-03-03 22:51:45 -0500

    added template index.html

--- src/main.py
+++ src/main.py
@@ -1,16 +1,40 @@
 import webapp2
+import os
+import jinja2
 
 from google.appengine.api import users
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname
+(__file__)))
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-
-        if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Hello,asdfasdf ' + user.nickname())
+        
+        if user:        
+            template_values = {
+                'name': user.nickname()
+            }
+            
+            template = jinja_environment.get_template('index.html')
+            self.response.out.write(template.render(template_values))
         else:
             self.redirect(users.create_login_url(self.request.uri))
+        
+#        user = users.get_current_user()
+#
+#        if user:
+#            self.response.headers['Content-Type'] = 'text/plain'
+#            self.response.out.write('Hello, ' + user.nickname())
+#        else:
+#            self.redirect(users.create_login_url(self.request.uri))
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+class TestPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('testing')
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/testing', TestPage)],
                               debug=True)
