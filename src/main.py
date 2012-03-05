848894235601ba848bf65e30b0bd18523255e6eb
Albert <albertyfwu@gmail.com>
2012-03-04 21:25:59 -0500

    improved layout

--- src/main.py
+++ src/main.py
@@ -4,8 +4,6 @@ import jinja2
 
 from google.appengine.api import users
 
-from apiclient.discovery import build
-
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname
 (__file__)))
@@ -17,7 +15,7 @@ class MainPage(webapp2.RequestHandler):
         if user:
             template_values = {
                 'username': user.nickname(),
-                'logOutUrl': users.create_logout_url("/")
+                'signOutUrl': users.create_logout_url("/")
             }
             
             template = jinja_environment.get_template('index.html')
